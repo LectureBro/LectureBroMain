2c5a2071f0838462e0a76d4c450b74e4222982b7
mtrzaska <loakie1337@gmail.com>
2025-01-11 21:37:54 +0100

    Configure backend, integrate backend with frontend, add endpoints, add .gitignore file and environment configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,6 +15,15 @@ export default defineConfig({
       ],
     }),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
